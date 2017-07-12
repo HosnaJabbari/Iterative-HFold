ef112ae063afd9af6e7375477cf5b7a18d13e4ce
IanWark <wark@ualberta.ca>
2017-07-12 15:47:15 -0600

    fix typo

--- simfold/src/common/common.cpp
+++ simfold/src/common/common.cpp
@@ -420,7 +420,7 @@ double compute_pf_sensitivity (char *ref_structure, s_partition_function *part, 
 
 
 
-void giveup (cosnt char *string1, const char *string2)
+void giveup (const char *string1, const char *string2)
 // to add: variable nb of parameters, as in scanf, printf
 {
     char temp[100];
