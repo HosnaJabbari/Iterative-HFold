afe2f89d2239dc55fe626ebda16a787640e5c533
Mahyar Hosseini <mahyar.hosseini@ualberta.ca>
2017-07-31 16:28:50 -0600

    Ignore non-negetive calculated energy,since the energy of the input is non-positive

--- HFold_iterative.cpp
+++ HFold_iterative.cpp
@@ -249,37 +249,30 @@ int main (int argc, char **argv) {
         method4_calculation(sequence, structure, method4_structure, method4_energy, result);
 
 
-        //Zero energy could be a valid result and it is not ingnored in the original perl code.
-        //So, we take it into account.
-        //if (*method1_energy < final_energy && *method1_energy != 0) {
-        if (*method1_energy < final_energy) {
+        //We ignore non-negetive energy, only if the energy of the input sequnces are non-positive!
+        //if (*method1_energy < final_energy) {
+        if (*method1_energy < final_energy && *method1_energy != 0) {
                 final_energy = *method1_energy;
                 strcpy(final_structure, method1_structure);
                 method_chosen = 1;
         }
 
-        //Zero energy could be a valid result and it is not ingnored in the original perl code.
-        //So, we take it into account.
-        //if (*method2_energy < final_energy && *method2_energy != 0) {
-        if (*method2_energy < final_energy) {
+        //if (*method2_energy < final_energy) {
+        if (*method2_energy < final_energy && *method2_energy != 0) {
                 final_energy = *method2_energy; 
                 strcpy(final_structure, method2_structure);             
                 method_chosen = 2;
         }
 
-        //Zero energy could be a valid result and it is not ingnored in the original perl code.
-        //So, we take it into account.
-        //if (*method3_energy < final_energy && *method3_energy != 0) {
-        if (*method3_energy < final_energy) {
+        //if (*method3_energy < final_energy) {
+        if (*method3_energy < final_energy && *method3_energy != 0) {
                 final_energy = *method3_energy; 
                 strcpy(final_structure, method3_structure);             
                 method_chosen = 3;
         }
 
-        //Zero energy could be a valid result and it is not ingnored in the original perl code.
-        //So, we take it into account.
-        //if (*method4_energy < final_energy && *method4_energy != 0) {
-        if (*method4_energy < final_energy) {
+       // if (*method4_energy < final_energy) {
+        if (*method4_energy < final_energy && *method4_energy != 0) {
                 final_energy = *method4_energy; 
                 strcpy(final_structure, method4_structure);             
                 method_chosen = 4;
