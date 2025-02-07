725ae633519eecc6c6246c137176b87e1bbab5f0
Mateo Gray <mgray7@beluga3.int.ets1.calculquebec.ca>
2025-02-07 03:01:49 -0500

    changed default to DP09

--- src/Iterative-HFold.cpp
+++ src/Iterative-HFold.cpp
@@ -325,7 +325,7 @@ int main (int argc, char *argv[])
 	validateSequence(seq);
 	if(restricted != "") validateStructure(seq,restricted);
 
-	std::string file= args_info.paramFile_given ? parameter_file : "params/rna_Turner2004.par";
+	std::string file= args_info.paramFile_given ? parameter_file : "params/rna_DirksPierce09.par";
 	if(exists(file)){
 		vrna_params_load(file.c_str(), VRNA_PARAMETER_FORMAT_DEFAULT);
 	}
